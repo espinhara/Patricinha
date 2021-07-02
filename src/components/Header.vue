1149a30dc121d22cd1bd3a19fbf2b94d57e19ee5
espinhara <gabrielespinhara13@gmail.com>
2021-07-02 09:21:33 -0300

    add item products in menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,6 +25,13 @@
       >
         <h1>Início</h1>
       </a>
+      <div class="subnav">
+        <h1 class="subnavbtn">Produtos <i class="fa fa-caret-down"></i></h1>
+        <div class="subnav-content">
+          <a href="/#olenka">Olenka</a>
+          <a href="/#muy_biela">Muy Biela</a>
+        </div>
+      </div>
       <a
         href="/#quemsomos"
         style="text-decoration: none"
@@ -48,4 +55,49 @@ export default {};
 </script>
 
 <style>
+  .subnav {
+  float: left;
+  overflow: hidden;
+}
+.subnav .subnavbtn {
+  /* margin: 0; */
+  /* border: none; */
+  color: #faf0f0;
+  transition: 0.3s;
+  border-top: 1px solid #000;
+  border-bottom: 1px solid #000;
+  padding: 0.5rem 0;
+  margin-right: 1rem;
+  background: #000;
+}
+.subnav .subnavbtn:hover{
+  color: #dea443;
+  border-color: #dea443;
+}
+.subnav-content {
+  display: none;
+  position: absolute;
+  /* left: 0; */
+  /* background-color: red; */
+  width: 100%;
+  z-index: 1;
+}
+.subnav-content a {
+  float: left;
+  color: white;
+  text-decoration: none;
+  color: #dea443;
+  border-color: #dea443;
+}
+
+/* Add a grey background color on hover */
+.subnav-content a:hover {
+  color: #dea443;
+  border-color: #dea443;
+}
+
+/* When you move the mouse over the subnav container, open the subnav content */
+.subnav:hover .subnav-content {
+  display: block;
+}
 </style>
